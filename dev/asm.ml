7de85c1be8f9c6b3aac58d4b2e882c5b0be69d37
Alfonzone <alfonsov96@gmail.com>
2021-09-21 22:59:15 -0300

    improved compile and asm

--- dev/asm.ml
+++ dev/asm.ml
@@ -3,31 +3,64 @@ open Printf
 (* registers *)
 type reg = 
 | RAX
+| RSP
+
+(* tags *)
+type tag =
+| Tag of string
+| IfTrue of int64
+| IfFalse of int64
+| IfDone of int64
 
 (* arguments for instructions *)
 type arg =
 | Const of int64
 | Reg of reg
+| StackPtr of reg * int64
 
 (* asm instructions *)
 type instruction =
 | IRet
 | IMov of arg * arg
+| IAdd of arg * arg
+| ISub of arg * arg
+| IAnd of arg * arg
+| ICmp of arg * arg
+| IJmp of tag
+| IJe of tag
+| IJle of tag
 (* TO BE COMPLETED *)
 
 let pp_reg reg : string =
   match reg with
   | RAX -> "RAX"
+  | RSP -> "RSP"
 
 let pp_arg arg : string =
   match arg with
   | Const n -> sprintf "%#Lx" n
   | Reg r -> pp_reg r
+  | StackPtr (r, n) -> sprintf "[%s - %Ld]" (pp_reg r) (Int64.mul 8L n)
+
+let pp_tag tag : string =
+  match tag with
+  | Tag t -> t
+  | IfTrue n -> sprintf "if_true_%Ld" n
+  | IfFalse n -> sprintf "if_false_%Ld" n
+  | IfDone n -> sprintf "if_done_%Ld" n
+
 
 let pp_instr instr : string =
   match instr with
   | IRet -> "  ret" 
   | IMov (a1, a2) -> sprintf "  mov %s, %s" (pp_arg a1) (pp_arg a2)
+  | IAdd (a1, a2) -> sprintf "  add %s, %s" (pp_arg a1) (pp_arg a2)
+  | ISub (a1, a2) -> sprintf "  sub %s, %s" (pp_arg a1) (pp_arg a2)
+  | IAnd (a1, a2) -> sprintf "  and %s, %s" (pp_arg a1) (pp_arg a2)
+  | ICmp (a1, a2) -> sprintf "  cmp %s, %s" (pp_arg a1) (pp_arg a2)
+  | IJmp tg -> sprintf "  jmp %s" (pp_tag tg)
+  | IJe tg -> sprintf "  je %s" (pp_tag tg)
+  | IJle tg -> sprintf "  jle %s" (pp_tag tg)
   (* TO BE COMPLETED *)
 
 let pp_instrs (instrs : instruction list) : string =
--- dev/compile.ml
+++ dev/compile.ml
@@ -1,9 +1,50 @@
 open Ast
 open Asm
 
-let rec compile_expr (e : expr) : instruction list =
+(* Environment *)
+type env = (string * int64) list
+let empty_env : env = []
+let extend_env : string -> int64 -> env -> env =
+  fun x v env -> (x, v) :: env
+
+let rec env_lookup (var : string) (env : env) : int64 =
+  match env with
+  | [] -> failwith "Unbound identifier."
+  | (n, slot) :: rest -> if n = var then slot else (env_lookup var rest)
+
+let bool_to_int (b : bool) : int64 =
+  if b then 0xA000000000000001 else 0xA000000000000000
+
+
+let binop_to_instr (op : prim2) (slot : int64) : instruction =
+  let params = (Reg RAX, StackPtr (RSP slot)) in
+  match op with 
+  | Add -> IAdd params
+  | And -> IAnd params
+
+
+let rec compile_expr (e : expr) (env : env) (slot : int64) : instruction list =
   match e with 
+  | Id s -> [IMov (Reg RAX, env_lookup s env)]
   | Num n -> [ IMov (Reg RAX, Const n) ] 
+  | Bool b -> [IMov (Reg RAX, Const (bool_to_int b))]
+  | Prim1 (op, n) -> begin match op with
+                        | Add1 -> [compile_expr n env slot] @ [IAdd (Reg RAX, Const 1)]
+                        | Sub1 -> [compile_expr n env slot] @ [ISub (Reg RAX, Const 1)]
+                  end
+  | Let (x, v, e) -> let new_env : env = extend_env x slot in
+                  [compile_expr v env slot]
+                @ [IMov (StackPtr (RSP, slot), Reg RAX)]
+                @ [compile_expr e new_env (slot + 1)]
+  | Prim2 (op, n1, n2) -> [compile_expr n1 env slot] @ [IMov (StackPtr (RSP, slot), Reg RAX)]
+                    @ [compile_expr n2 env (slot + 1)] @ [IMov (StackPtr (RSP, (slot + 1)), Reg RAX)]
+                    @ [IMov (Reg RAX, StackPtr (RSP, slot))]
+                    @ (begin match op with 
+                             | Lte -> [ICmp (Reg RAX, StackPtr (RSP, (slot + 1)))]
+                                    @ [IJle (IfTrue, IfFalse)] 
+                             | _ -> [(binop_to_instr op (slot + 1))]
+                      end)
+                          
   | _ -> failwith "TO BE DONE!"
 
 let compile e : string =
