fd6fb268cf896d26892e1920a8471df5d5a1cd04
Alfonzone <alfonsov96@gmail.com>
2021-10-25 17:42:04 -0300

    test legibility improved

--- tests/testentrega3.ml
+++ tests/testentrega3.ml
@@ -74,53 +74,39 @@ let test_tag_set () =
 
 let test_compile_tuple () =
   check instruction_list "same tuple" (compile_expr (tag_expr (Tuple ([Id "arg1"; Id "arg2"]))) [("arg2", -2L) ; ("arg1", -1L)] (Int64.sub 0L 3L) empty_comp_fenv 0 0)
-                         ([
-                          iPush r11 ;
-                          iMov_arg_arg r11 r15 ;
-                          iMov_arg_const (Ptr (R11, 0L)) (8L(*Int64.of_int (4 * (List.length attrs))*)) ;
-                          iAdd_arg_const r15 (24L(*Int64.of_int (8 * (List.length attrs + 1))*))
-                        ] @
-                        (*(compile_lexpr lst 1) (* <=> *)*)
-                        [
-                          iMov_arg_to_RAX (Ptr(RBP, -1L))
-                        ] @
-                        (*(compile_expr hd slot_env next_slot fenv tag_fun total_params)*) 
-                        [iMov_arg_arg (Ptr (R11, (1L(*Int64.of_int param_pos*)))) rax] @
-                        [
-                          iMov_arg_to_RAX (Ptr(RBP, -2L))  
-                        ]
-                        (*(compile_expr hd slot_env next_slot fenv tag_fun total_params)*) @
-                        [iMov_arg_arg (Ptr (R11, (2L(*Int64.of_int param_pos*)))) rax]
-                        (*(compile_lexpr tl (param_pos + 1))*) @
-                        [
-                          iMov_arg_to_RAX r11 ;
-                          iAdd_arg_const rax 1L ;
-                          iPop r11
-                        ] @
-                        [
-                          iAdd_arg_const r15 7L ;
-                          iPush r11 ;
-                          iMov_arg_const r11 (Int64.mul Int64.minus_one 8L) ;
-                          iAnd_arg_arg r15 r11 ; 
-                          iPop r11
-                        ]
+                        (
+                          [
+                            iPush r11 ;
+                            iMov_arg_arg r11 r15 ;
+                            iMov_arg_const (Ptr (R11, 0L)) 8L ;
+                            iAdd_arg_const r15 24L ;
+                            iMov_arg_to_RAX (Ptr(RBP, -1L)) ;
+                            iMov_arg_arg (Ptr (R11, (1L))) rax ;
+                            iMov_arg_to_RAX (Ptr(RBP, -2L)) ;
+                            iMov_arg_arg (Ptr (R11, 2L)) rax ;
+                            iMov_arg_to_RAX r11 ;
+                            iAdd_arg_const rax 1L ;
+                            iPop r11 ;
+                            iAdd_arg_const r15 7L ;
+                            iPush r11 ;
+                            iMov_arg_const r11 (Int64.mul Int64.minus_one 8L) ;
+                            iAnd_arg_arg r15 r11 ; 
+                            iPop r11
+                          ]
                         )
 
 let test_compile_get () =
   check instruction_list "same get" (compile_expr (tag_expr (Prim2 (Get, Id "x", Id "y"))) [("x", -1L) ; ("y", -2L)] (Int64.sub 0L 3L) empty_comp_fenv 0 0)
-                       (
-                        let r10_ptr = Ptr(R10, 0L) in
-                        
-                        [ iMov_arg_to_RAX (Ptr(RBP, -1L)) ] @
-                        [ iMov_arg_arg (Ptr(RBP, -3L)) rax] @
-                        [ iMov_arg_to_RAX (Ptr(RBP, -2L)) ] @
-                        [ iMov_arg_arg (Ptr(RBP, -4L)) rax ] @
-                        [ iMov_arg_to_RAX (Ptr(RBP, -3L))] @
-                        [
+                      (
+                        [ 
+                          iMov_arg_to_RAX (Ptr(RBP, -1L))  ;
+                          iMov_arg_arg (Ptr(RBP, -3L)) rax ;
+                          iMov_arg_to_RAX (Ptr(RBP, -2L))  ;
+                          iMov_arg_arg (Ptr(RBP, -4L)) rax  ;
+                          iMov_arg_to_RAX (Ptr(RBP, -3L)) ;
                           iPush r10 ;
                           iPush r11 ;
                           iMov_arg_arg r10 rax
-                          
                         ] @
                         (check_rax_is_tuple_instr (Int64.sub (Int64.sub 0L 4L) 1L)) @
                         [
@@ -129,33 +115,35 @@ let test_compile_get () =
                           iMov_arg_to_RAX (Ptr(RBP, -4L))
                         ] @
                         (check_rax_is_int_instr (Int64.sub 0L 4L)) @
-                        [iMov_arg_arg r11 (Ptr(RBP, -4L))] @
                         [
+                          iMov_arg_arg r11 (Ptr(RBP, -4L)) ;
                           iCmp_arg_const r11 0L ;
                           IJl "index_too_low" ;
-                          iCmp_arg_arg r11 r10_ptr ;
+                          iCmp_arg_arg r11 (Ptr(R10, 0L)) ;
                           IJge "index_too_high" ;
                           iSar r11 2L ;
                           iAdd_arg_const r11 1L ;
                           iSal r11 3L ;
                           iAdd_arg_arg r10 r11 ;
-                          iMov_arg_to_RAX r10_ptr ;
+                          iMov_arg_to_RAX (Ptr(R10, 0L)) ;
                           iPop r11 ;
                           iPop r10
                         ] 
-                       )
+                      )
   
 let test_compile_set () =
   check instruction_list "same set" (compile_expr (tag_expr (Set (Id "t", Id "p", Id "x"))) [("x", -3L) ; ("p", -2L) ; ("t", -1L)] (Int64.sub 0L 4L) empty_comp_fenv 0 0)
                        (
                         [ iMov_arg_to_RAX (Ptr(RBP, -1L)) ] @
                         (check_rax_is_tuple_instr (-4L)) @
-                        [ iMov_arg_to_RAX (Ptr(RBP, -2L)) ;
+                        [ 
+                          iMov_arg_to_RAX (Ptr(RBP, -2L)) ;
                           iMov_arg_arg (rbp_pointer (-5L)) rax ; 
                           iMov_arg_arg rax (rbp_pointer (-5L))
                         ] @
                         (check_rax_is_int_instr (-5L)) @
-                        [ iMov_arg_to_RAX (Ptr(RBP, -3L)) ;
+                        [ 
+                          iMov_arg_to_RAX (Ptr(RBP, -3L)) ;
                           iMov_arg_arg (rbp_pointer (Int64.sub (-5L) 1L)) rax ;
                           iPush r10 ;
                           iPush r11 ;
@@ -176,11 +164,4 @@ let test_compile_set () =
                           iPop r10 ;
                           iMov_arg_to_RAX (Ptr(RBP, -4L))(*rbp_ptr*)
                         ]
-                       )
-      
-(*let test_compile_set () =
-  check arg "same set" (compile_expr (tag_prog (Set (Id "x", 0, Num 5L)) empty_slot_env (Int64.sub 0L 2L) empty_comp_fenv 0 0)
-                       ([fetch_to_stack -1 ; check_index_range ; move_to_RAX (Num 20L) ; set_RAX_on_heap 2])*)
-
-
-                      
+                      )
